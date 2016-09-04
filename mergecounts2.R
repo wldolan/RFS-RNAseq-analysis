c9bb54e51657f089383162cb8088719dc912c267
Whitney Dolan <whitney.l.dolan@gmail.com>
2016-09-04 11:34:59 -0400

    Create mergecounts2.R

---
+++ mergecounts2.R
@@ -0,0 +1,35 @@
+## used for merging countfiles (output from htseq-count)
+## Whitney Dolan (whitneyldolan@gmail.com)
+## last updated 2016-03-09
+
+#set working directory
+setwd("/Users/whitneydolan/Documents/ChappleLab/temp/results_round3/countfiles/")
+
+#Combine with Reduce to merge lists
+#Reduce(ManyMerge, list)
+ManyMerge<- function(x, y){
+  df<- merge(x, y, by= 0, all.x= T, all.y= T)
+  rownames(df)<- df$Row.names
+  df$Row.names<- NULL
+  return(df)
+}
+
+#list of individual countfiles
+countfiles<-list.files("./") 
+
+#load count file with gene names as rownames, and filename as header for counts
+load.file <-function(filename) {
+  counttable <-read.table(filename, row.names=1, col.names=c("gene",filename))
+  }
+
+#process all count files
+allcounts <-lapply(countfiles, load.file)
+
+#merge all count files into one
+mergedcounts<-Reduce(ManyMerge, allcounts)
+
+#save merged files
+write.csv(mergedcounts, "./allcounts.csv")
+
+#save counts without summary statistics 
+write.csv(mergedcounts[6:nrow(mergedcounts),], "./justcounts.csv")
